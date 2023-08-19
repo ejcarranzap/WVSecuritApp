e8cf0965a513ff272a472a0f4b491fce79c313e5
jonatanc08 <ejcarranzap@gmail.com>
2023-08-18 18:30:21 -0600

    .

--- app/src/main/java/com/itx/wvsecurit/tool/MyDialog.kt
+++ app/src/main/java/com/itx/wvsecurit/tool/MyDialog.kt
@@ -2,6 +2,7 @@ package com.itx.wvsecurit.tool
 
 import android.app.AlertDialog
 import javax.inject.Inject
+import javax.inject.Singleton
 
 class MyDialog  @Inject constructor(private val alertDialog: AlertDialog) {
     fun show(){
